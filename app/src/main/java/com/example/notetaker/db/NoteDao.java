6101140e31ed335730012ee20cf42db26ff2bebc
Izzy3145 <izzy.stannett@gmail.com>
2020-04-07 18:23:35 +0100

    Room db added

---
+++ app/src/main/java/com/example/notetaker/db/NoteDao.java
@@ -0,0 +1,34 @@
+package com.example.notetaker.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.notetaker.models.Note;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+public interface NoteDao {
+
+    //using @Insert instructs Room to insert all parameters into the db in a single transaction
+    @Insert
+    Single<Long> insertNote(Note note) throws Exception;
+
+    //the @Update annotation modifies the set of entities given as parameters in the db
+    //it uses a query that matches against the primary key of each entry in the entity
+    @Update
+    Single<Integer> updateNote(Note note) throws Exception;
+
+    //the @Delete annotation deletes the entities passed in as parameters in the db
+    @Delete
+    Single<Integer> deleteNote(Note note) throws Exception;
+
+    //@Query allows read/write operations on the db. Each @Query method is verified at compile time
+    //Room also verifies the return type if the fields in the returned object don't match the entity column names
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+ }
--- app/src/test/java/com/example/notetaker/DateUtilsTest.java
+++ app/src/test/java/com/example/notetaker/DateUtilsTest.java
@@ -1,8 +1,12 @@
 package com.example.notetaker;
 
 
+import com.example.notetaker.util.DateUtil;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,9 +16,25 @@ import static com.example.notetaker.util.DateUtil.GET_MONTH_ERROR;
 import static com.example.notetaker.util.DateUtil.getMonthFromNumber;
 import static com.example.notetaker.util.DateUtil.monthNumbers;
 import static com.example.notetaker.util.DateUtil.months;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DateUtilsTest {
+
+    private static String today = "04-2020";
+    @Test
+    void getCurrentTimestamp_assertNoException_returnTrue() {
+        //assertDoesNotThrow takes an executable
+        //and executable is like a Runnable, but it can throw any kind of exception
+        assertDoesNotThrow(new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                assertEquals(today, DateUtil.getCurrentTimeStamp());
+                System.out.println("Timestamp is generated correctly");
+
+            }
+        });
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {0,1,2,3,4,5,6,7,8,9,10,11})
